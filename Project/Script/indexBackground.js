78c86be9a7d3cfa4b18c7edeb0f909f6f3e22484
Landeiras <landeirasluis@gmail.com>
2022-05-01 15:11:25 +0100

    In Process

---
+++ Project/Script/indexBackground.js
@@ -0,0 +1,18 @@
+function background(){
+    var button = document.getElementById("tema");
+    if(button.checked){
+        document.body.style = "background: linear-gradient(white 30%, rgb(94, 2, 89));";
+        document.getElementById("bar").style.backgroundColor = "rgba(255, 255, 255, 0.89)";
+        document.getElementById("box").style.backgroundColor = "rgba(255, 255, 255, 0.89)";
+        document.getElementById("button").style.backgroundColor = "rgba(0, 0, 0, 0.89)";
+        document.getElementById("h3").style.color = "black"; 
+        document.getElementById("ou").style.color = "black";  
+    }else{
+        document.body.style = "background: linear-gradient(black 30%, rgb(94, 2, 89));";
+        document.getElementById("bar").style.backgroundColor = "rgba(0, 0, 0, 0.89)";
+        document.getElementById("box").style.backgroundColor = "rgba(0, 0, 0, 0.89)";
+        document.getElementById("button").style.backgroundColor = "rgba(255, 255, 255, 0.89)";
+        document.getElementById("h3").style.color = "white"; 
+        document.getElementById("ou").style.color = "white";    
+    }
+}
