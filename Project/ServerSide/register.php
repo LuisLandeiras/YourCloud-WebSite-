39bc50346c1d0c8e9b7b632ae4e8b4955ae66bda
Landeiras <landeirasluis@gmail.com>
2022-05-04 19:48:15 +0100

    dasd

---
+++ Project/ServerSide/register.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    header('location: ../ClientSide/login.html');
+
+    $db = mysqli_connect('localhost', 'root', '', 'test');
+
+    $name = $_POST['Nome'];
+    $username = $_POST['UserName'];
+    $password = $_POST['Pass'];
+    $pass = $_POST['PassConf'];
+
+    $sql = "SELECT * FROM registos WHERE UserName = '$username'";
+    $result = mysqli_query($db, $sql);
+    $num = mysqli_num_rows($result);
+
+    if($pass !== $password){
+        echo "Passwords do not match";
+        header('location: ../register.html');
+        mail($email,$assunto,$mens,$headers);   
+    }
+    else if($num == 1){
+        echo "Username already exists";
+    }
+    else{
+        $sql = "INSERT INTO registos (Nome, UserName, Pass) VALUES ('$name', '$username', '$password')";
+        mysqli_query($db, $sql);
+        $_SESSION['success'] = "You are now logged in";
+    }
+?>
