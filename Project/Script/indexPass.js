1b6ec48b258030eedb3e72847b0caf2a7e4df8a1
ReynaRodriguess <reynarodriguess123@gmail.com>
2022-05-04 22:39:55 +0100

    not finished

--- Project/Script/indexPass.js
+++
@@ -1,7 +0,0 @@
-function equal(){
-    var a = document.getElementById("pass").value;
-    var b = document.getElementById("repitepass").value;
-    if(b != a){
-        alert("É necessário que as senhas sejam iguais");
-    }
-}
