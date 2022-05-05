9656698bbc8477652570a806f8d1339abe31e506
LuisLandeiras <landeirasluis@gmail.com>
2022-05-05 09:50:01 +0100

    Update home.php

--- Project/ServerSide/home.php
+++ Project/ServerSide/home.php
@@ -10,10 +10,25 @@
 	<title>Home</title>
 </head>
 <body>
-
-<div class="header">
-	<h2>Home Page</h2>
-</div>
-	<a href="logout.php">Logout</a>	
+	<nav class="navbar" id="bar">
+        <a href="index.php" id="logo" target=_blank>YourCloud</a>
+        <input type="checkbox" id="toggler">
+        <label for="toggler"><i class="ri-menu-line"></i></label>
+        <div class="menu">
+            <ul class="list">
+                <li><a href="sobre.php" target=_blank>Sobre Nós</a></li>
+				<li><a href="logout.php">Logout</a></li>
+                <li>
+                    <input type="checkbox" id="tema" onclick="background()">
+                    <label for="tema" class="button" id="button"></label>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <br>
+	<menu>
+		<li><input type="checkbox" id="pdf" ><label for="pdf">PDF</label></li>
+		<li><input type="checkbox" id="mp3"><label for="mp3">MP3</label></li>
+	</menu>
 </body>
 </html>
